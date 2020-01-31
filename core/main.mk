19462b3712671d3acad57d40a697d3879910c643
tuxafgmur <tuxafgmur@users.noreply.github.com>
2020-01-31 03:32:24 +0100

    main: changes secure options for userdebug builds

--- core/main.mk
+++ core/main.mk
@@ -279,32 +279,27 @@ endif
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-  # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
-  ifeq ($(user_variant),user)
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
-  endif
-
   ifeq ($(user_variant),userdebug)
-    # Pick up some extra useful tools
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
     tags_to_install += debug
   else
-    # Disable debugging in plain user builds.
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
     enable_target_debugging :=
   endif
 
-  # Disallow mock locations by default for user builds
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
-
 else # !user_variant
-  # Turn on checkjni for non-user builds.
-  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
-  # Set device insecure for non-user builds.
+
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
-  # Allow mock locations by default for non user builds
   ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
+
 endif # !user_variant
 
 ifeq (true,$(strip $(enable_target_debugging)))
